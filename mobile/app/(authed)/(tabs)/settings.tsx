ebe1d56dfc740a943ad3f2dd05cb1e8ad39c23a6
yenilikci <m.melihcelik017@outlook.com>
2024-09-07 19:40:14 +0300

    feat(settings): updated settings screen and add logout button

--- mobile/app/(authed)/(tabs)/settings.tsx
+++ mobile/app/(authed)/(tabs)/settings.tsx
@@ -1,5 +1,13 @@
+import { useAuth } from '@/context/AuthContext';
+import { VStack } from '@/components/VStack';
+import { Button } from '@/components/Button';
+
 export default function SettingsScreen() {
+    const { logout } = useAuth();
+
     return (
-        <></>
-    )
+        <VStack flex={1} m={20}>
+            <Button onPress={logout}>Logout</Button>
+        </VStack>
+    );
 }
