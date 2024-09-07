0c3c6d36233464242e02d05bd55f56c7d8976b50
yenilikci <m.melihcelik017@outlook.com>
2024-09-07 23:03:47 +0300

    feat(scan-ticket): added QR code scanning and validation for tickets

--- mobile/app/(authed)/(tabs)/(tickets)/ticket/[id].tsx
+++ mobile/app/(authed)/(tabs)/(tickets)/ticket/[id].tsx
@@ -1,5 +1,59 @@
+import { Text } from "@/components/Text";
+import { VStack } from "@/components/VStack";
+import { ticketService } from "@/services/ticket";
+import { Ticket } from "@/types/ticket";
+import { useFocusEffect } from "@react-navigation/native";
+import { router, useLocalSearchParams, useNavigation } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { Image } from "react-native";
+
 export default function TicketDetailScreen() {
+    const navigation = useNavigation();
+    const { id } = useLocalSearchParams();
+
+    const [ticket, setTicket] = useState<Ticket | null>(null);
+    const [qrcode, setQrcode] = useState<string | null>(null);
+
+    async function fetchTicket() {
+        try {
+            const { data } = await ticketService.getOne(Number(id));
+            setTicket(data.ticket);
+            setQrcode(data.qrcode);
+        } catch (error) {
+            router.back();
+        }
+    }
+
+    useFocusEffect(useCallback(() => { fetchTicket(); }, []));
+
+    useEffect(() => {
+        navigation.setOptions({ headerTitle: "" });
+    }, [navigation]);
+
+    if (!ticket) return null;
+
     return (
-        <></>
-    )
+        <VStack
+            alignItems="center"
+            m={20}
+            p={20}
+            gap={20}
+            flex={1}
+            style={{
+                backgroundColor: "white",
+                borderRadius: 20
+            }}
+        >
+            <Text fontSize={32} bold>{ticket.event.name}</Text>
+            <Text fontSize={20} bold>{ticket.event.location}</Text>
+            <Text fontSize={16} color="gray">{new Date(ticket.event.date).toLocaleString()}</Text>
+
+            <Image
+                style={{ borderRadius: 20 }}
+                width={300}
+                height={300}
+                source={{ uri: `data:image/png;base64,${qrcode}` }}
+            />
+        </VStack>
+    );
 }
--- mobile/app/(authed)/(tabs)/scan-ticket/_layout.tsx
+++ mobile/app/(authed)/(tabs)/scan-ticket/_layout.tsx
@@ -1,5 +1,61 @@
+import { Button } from '@/components/Button';
+import { Text } from '@/components/Text';
+import { VStack } from '@/components/VStack';
+import { ticketService } from '@/services/ticket';
+import { BarcodeScanningResult, CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { ActivityIndicator, Alert, Vibration } from 'react-native';
+
 export default function ScanTicketScreen() {
+    const [permission, requestPermission] = useCameraPermissions();
+    const [scanningEnabled, setScanningEnabled] = useState(true);
+
+    if (!permission) {
+        return <VStack flex={1} justifyContent='center' alignItems='center'>
+            <ActivityIndicator size={"large"} />
+        </VStack>;
+    }
+
+    if (!permission.granted) {
+        return (
+            <VStack gap={20} flex={1} justifyContent='center' alignItems='center'>
+                <Text>Camera access is required to scan tickets.</Text>
+                <Button onPress={requestPermission}>Allow Camera Access</Button>
+            </VStack>
+        );
+    }
+
+    async function onBarcodeScanned({ data }: BarcodeScanningResult) {
+        if (!scanningEnabled) return;
+
+        try {
+            Vibration.vibrate();
+            setScanningEnabled(false);
+
+            const [ticket, owner] = data.split(",");
+            const ticketId = parseInt(ticket.split(":")[1]);
+            const ownerId = parseInt(owner.split(":")[1]);
+
+            await ticketService.validateOne(ticketId, ownerId);
+
+            Alert.alert('Success', "Ticket validated successfully.", [
+                { text: 'Ok', onPress: () => setScanningEnabled(true) },
+            ]);
+
+        } catch (error) {
+            Alert.alert('Error', "Failed to validate ticket. Please try again.");
+            setScanningEnabled(true);
+        }
+    }
+
     return (
-        <></>
-    )
+        <CameraView
+            style={{ flex: 1 }}
+            facing="back"
+            onBarcodeScanned={onBarcodeScanned}
+            barcodeScannerSettings={{
+                barcodeTypes: ["qr"],
+            }}
+        />
+    );
 }
